20d6c1f322a339c79cecdd1d26093b998ff658a1
ExIxIxS <denis.bondarenko.pl@gmail.com>
2023-03-21 12:24:36 +0100

    feat: add main content

--- src/pages/playlist-page/scripts/playlist-page.js
+++ src/pages/playlist-page/scripts/playlist-page.js
@@ -2,3 +2,30 @@
 /// Here we import all the JavaScript files we need for our homepage.
 
 import '../styles/playlist-page.scss'
+
+fetch('./src/pages/playlist-page/assets/json/playlist.json')
+    .then(response =>  response.json())
+    .then(playlist => {
+      document.querySelector('.playlist-page-main-component__playlist')
+        .appendChild(createPlaylistContent(playlist));
+    })
+    .catch((e) => console.log(e));
+
+
+function createPlaylistContent(playlist) {
+  const fragment = document.createDocumentFragment();
+  const songItemTemp = document.querySelector('#song-template');
+
+  playlist.forEach((song) => {
+    const songClone = songItemTemp.content.cloneNode(true);
+    songClone.querySelector('.playlist-page-main-component__song-number').textContent = song.order;
+    songClone.querySelector('.playlist-page-main-component__title').textContent = song.songTitle;
+    songClone.querySelector('.playlist-page-main-component__subtitle').textContent = song.albumTitle;
+    songClone.querySelector('.playlist-page-main-component__title--artist').textContent = song.artistName;
+    songClone.querySelector('.playlist-page-main-component__title--time').textContent = song.duration;
+    songClone.querySelector('.playlist-page-main-component__album-cover-img').src = song.coverLink;
+    fragment.append(songClone);
+  });
+
+  return fragment;
+}
